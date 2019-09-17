78f03f052a63cde13bdaeef62af3b6a40d97393f
Шамиль <internetgggg@mail.ru>
2019-09-17 07:14:11 +0300

    Создает функцию getMaxElement

--- js/stat.js
+++ js/stat.js
@@ -17,6 +17,17 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    maxElement = arr[i] > maxElement ? arr[i] : maxElement;
+  }
+
+  return maxElement;
+};
+
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)'); // Рисует белое облако
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff'); // Рисует тень облако
@@ -26,13 +37,10 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 42);
   ctx.fillText('Список результатов:', 120, 60);
 
-  var maxTime = 0;
-  for (var i = 0; i < times.length; i++) {
-    maxTime = times[i] > maxTime ? times[i] : maxTime;
-  }
-
+  var maxTime = getMaxElement(times);
   var interval = 0;
-  for (i = 0; i < names.length; i++) {
+
+  for (var i = 0; i < names.length; i++) {
     var randomNumber = Math.round(Math.random() * 100);
     ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'hsl(240,100%,' + randomNumber + '%)';
     ctx.fillRect(BAR_X + interval, BAR_Y, BAR_WIDTH, -(MAX_BAR_HEIGHT * times[i]) / maxTime);
