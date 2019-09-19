8bd22b19a0b964f691f95712f3149626701fbd33
Шамиль <internetgggg@mail.ru>
2019-09-19 19:14:33 +0300

    Завершение третьего задания

---
+++ js/setup.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг',
+];
+
+var MANTLE_COLOR = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)',
+];
+
+var EYE_COLOR = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green',
+];
+
+var parameter = ['name', 'name', 'coatColor', 'eyesColor'];
+
+var values = [WIZARD_NAMES, WIZARD_SURNAMES, MANTLE_COLOR, EYE_COLOR];
+
+var wizards = [];
+
+// ------Возвращает случайное число
+var getRandomNumber = function (arry) {
+  return Math.round(0 - 0.5 + Math.random() * ((arry.length - 1) - 0 + 1));
+};
+// //////////////////////////////////////////////////////
+
+// ----Создает Стрктуру данных
+var createsWizard = function () {
+  for (var i = 1; i <= 4; i++) {
+    var wizard = {name: ''};
+    for (var j = 0; j < 4; j++) {
+      if (parameter[j] === 'name') {
+        wizard[parameter[j]] += ' ' + values[j][getRandomNumber(values[j])];
+      } else {
+        wizard[parameter[j]] = values[j][getRandomNumber(values[j])];
+      }
+    }
+    wizards.push(wizard);
+  }
+  return wizards;
+};
+// //////////////////////////////////////////////////////////
+
+wizards = createsWizard();
+
+// ----------Вставляет данные в шаблон
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+// ///////////////////////////////////////////////////////////////
+
+// --------------Вставляет готовый шаблон в разметку
+var outputWizard = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+// //////////////////////////////////////////////////////
+
+outputWizard();
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
